87fea3f1076a20c9238fcbc66ca097d2b2364c1f
sagar <sagarrajput2111@gmail.com>
2025-01-10 01:13:40 +0530

    Register and Login page HTML Template Created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent }
+];
