7539063d8257196586cb5171fa4c1337243a0293
sagar <sagarrajput2111@gmail.com>
2025-01-10 22:32:50 +0530

    reactive form added for both register and login with model

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { LoginCreds } from './models/login.model';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  userCred!:LoginCreds;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = formBuilder.group({
+      username: ['', [Validators.required,Validators.email]],
+      password: ['', Validators.required]
+
+    })
+  }
+
+  onSubmit()
+  {
+    this.userCred=this.loginForm.value;
+    console.log(typeof(this.loginForm.value));
+    console.log(this.userCred)
+  }
 
 }
---
+++ src/app/login/models/login.model.ts
@@ -0,0 +1,5 @@
+export interface LoginCreds
+{
+    username:string;
+    password:string;
+}
